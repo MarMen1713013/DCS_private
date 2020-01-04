d0e6b50989b17786b0407747b63e60189285f7c1
MarMen1713013 <menchetti.1713013@studenti.uniroma1.it>
2020-01-04 14:35:18 +0100

    Squashed commit of the following:
    Author: andrea-1996
        Amplifier and prob
    	We read the outpuit voltage of the amplifier and the input current of the motor
    Author: andrea-1996
        Test load
    	In this script we read the voltage on the potenziometer and we use this voltage to supply the motor

---
+++ Matlab/test_load.m
@@ -0,0 +1,27 @@
+clear all
+clc
+close all
+
+%% Definitions
+a = arduino;
+configurePin(a,'A0', 'AnalogInput') ;  % Read pin for potenziometer
+configurePin(a,'D11', 'DigitalOutput')   % ENA PWM Set motor speed
+configurePin(a,'D12', 'DigitalOutput')   % IN1  1------0
+configurePin(a,'D13', 'DigitalOutput')   % IN2  0------1
+
+writeDigitalPin(a,'D12',0);
+writeDigitalPin(a,'D13',1);
+
+Y=[]
+while(1)
+    
+   pot = readVoltage(a,'A0');  % read analog voltage from potentiometer
+   writePWMVoltage(a,'D11',pot); 
+   Y=[Y;pot];
+   plot(Y)
+   
+end
+
+
+
+
