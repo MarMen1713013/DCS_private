6440fe2f607bf9aaa06fbd657a5ab1022eee0cc0
adrijasen1 <adrijasen01@gmail.com>
2020-02-09 10:13:28 +0100

    Create tau_mech.m

---
+++ Matlab/tau_mech.m
@@ -0,0 +1,40 @@
+clear
+clc
+close all
+
+%Arduino connections
+a = arduino('COM1','Uno','Libraries','rotaryEncoder')
+
+encoder = rotaryEncoder(a,'D8','D10',11)
+
+time = 0;
+data = 0;
+count = 0;
+rpm = 0;
+v_rpm = 0;
+
+figure();
+title("Speed response");
+xlabel("Time [s]");
+ylabel("Angular speed [RPM]")
+grid on
+hold on
+
+
+tic
+for count= 1:50
+    time(count) = toc;
+    rpm=readSpeed(encoder);
+    f_rpm (count)= rpm;
+    plot(time, f_rpm, '-')
+    
+   tau_mmech(count)= 0.63* f_rpm;
+end
+
+   sr_tau= sumsqr(tau_mmech);
+   disp(sr_tau);
+   lsa= sqrt((sr_tau)/count) ;
+   disp(lsa);
+
+
+
