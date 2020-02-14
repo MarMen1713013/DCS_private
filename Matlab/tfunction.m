ffbcda539edb8e005cb4859832b41150fef6f312
MarMen1713013 <menchetti.1713013@studenti.uniroma1.it>
2020-02-14 13:19:48 +0100

    I'm a f*cker

--- Matlab/tfunction.m
+++ Matlab/tfunction.m
@@ -9,7 +9,7 @@ B_n = B_mean;
 T_n = 20e-3;
 WrWe = (Ki/s/B)*((1+s*Kp/Ki)/(1+s*J/B));
 WrT = 1/B*(1/(1+s*J/B));
-WrWe_zoh = collect(expand(subs(WrWe,s,(z-1)*T)),z)
-WrT_zoh = collect(expand(subs(WrT,s,(z-1)*T)),z)
+WrWe_zoh = collect(expand(subs(WrWe,s,(z-1)/T)),z)
+WrT_zoh = collect(expand(subs(WrT,s,((z-1)/T))),z)
 
 WrWe_zoh_n = subs(WrWe_zoh,{Kp,Ki,J,B,T},{Kp_n,Ki_n,J_n,B_n,T_n})
