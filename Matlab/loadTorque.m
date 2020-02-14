3d3c626914569a28b363c5259803fa2d618bf099
MarMen1713013 <menchetti.1713013@studenti.uniroma1.it>
2020-02-14 13:02:23 +0100

    [dynamic load torque estimator] stuck

--- Matlab/loadTorque.m
+++ Matlab/loadTorque.m
@@ -1,24 +1,58 @@
+%% initialization
 clear;
 close all;
 clc;
-addpath '/home/marco/Scrivania/DCS/Matlab/utility_motor'
-addpath '/home/marco/Scrivania/DCS/Matlab/datas'
-load 'datas/B.mat'
-initDCS; % initialize the arduino and the motors
-sim_length = 1000; % number of samples of the simulation
+addpath utility_motor/
+addpath datas/
+load datas/B.mat
+load speedReadings.mat
+%initDCS;
+%% utility
+sim_length = 500;
 
-r_ratio = 40; % reduction ratio of the motor
+Ts = 20e-3;
+Ki = 1;
+K_poi = 0.09;
+Kp = K_poi*Ki;
 
-speed_meas = 0; % measurement of the speed
-
-t = [];
+%% motor param
+B = B_mean;
+J = 3.6e-3;
+%% quantities
+wr = zeros(3,1);
+we = zeros(3,1);
+save_wr = zeros(1,sim_length);
+save_we = save_wr;
+%% constants
+n1 = Kp*Ts;
+n2 = Ki-Kp*Ts;
+d0 = J*Ts^2;
+d1 = B*Ts-2*J*Ts^2;
+d2 = J*Ts^2-B*Ts;
+%% core
+t = zeros(1,sim_length);
+i = 1;
 tic
-bEmf = zeros(2,1);
-V = 3.3;
-Va = pwm2V(V)*ones(2,1);
-Ia = zeros(2,1);
-for i = 0:sim_length
-   t = [t;toc];
-   bEmf = readBackEmf(bEmf,encoder);
-   Ia = 
+while(t(i) <= 60 && i < sim_length)
+    %time delay
+    wr(3) = wr(2);
+    wr(2) = wr(1);
+    
+    we(3) = we(2);
+    we(2) = we(1);
+    
+    wm = speedReadings(i);
+    %wm = rpm2rad(abs(readSpeed(encoder)/40));
+    we(1) = wm - wr(1); % measure it
+    wr(1) = n1/d0*we(2) + n2/d0*we(3) - d1/d0*wr(2) - d2/d0*wr(3);
+    save_wr(i) = wr(1);
+    save_we(i) = we(1);
+%     plot(t(1:i),save_wr(1:i));
+%     pause(0.01)
+    t(i) = toc;
+    i = i+1;
 end
+
+plot(t,save_wr)
+figure()
+plot(t,save_we)
--- Matlab/test_amp.m
+++ Matlab/test_amp.m
@@ -1,21 +1,21 @@
-clear
+% clear
 close all
-clc
-addpath './utility_motor/'
-initDCS;
+% clc
+% addpath './utility_motor/'
+% initDCS;
 counterClockWise(a,main);
-in = zeros(1,1500);
+in = zeros(1,1000);
 t = in;
 tic
 
 for i=1:5
     go(a,5*(i-1)/4,main);
     disp(['pwm_voltage: ',num2str(5*(i-1)/4),'V']);
-    for j = 1:300
-        next = j + 300*(i-1);
+    for j = 1:200
+        next = j + 200*(i-1);
         in(next) = readVoltage(a,probe); % randn(1)*10 + 3;
         t(next) = toc;
-        plot(t(1:next),in(1:next));
+        plot(in(1:next));%plot(t(1:next),in(1:next));
         pause(0.01);
     end
 end
